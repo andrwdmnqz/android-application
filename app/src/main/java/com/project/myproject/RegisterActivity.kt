3b01ea114d58dfe8ecc64eccbbe7084ffbf77ef2
Andrii Shokhirev <andresshokhirev@gmail.com>
2024-03-08 13:34:52 +0200

    top part of second activity screen

---
+++ app/src/main/java/com/project/myproject/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.project.myproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInitialState: Bundle?) {
+        super.onCreate(savedInitialState)
+        setContentView(R.layout.register_activity)
+    }
+}
