791b9d4f99ac26ba473896f53ca346c3746e7bf3
Andrii Shokhirev <andresshokhirev@gmail.com>
2024-03-21 09:50:11 +0200

    delete item and show toast

--- app/src/main/java/com/project/myproject/adapters/UserAdapter.kt
+++ app/src/main/java/com/project/myproject/adapters/UserAdapter.kt
@@ -5,17 +5,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.project.myproject.R
 import com.project.myproject.extensions.loadImageByGlide
 import com.project.myproject.models.User
 
-class UserAdapter(var contactsList: List<User>) :
+class UserAdapter(var contactsList: ArrayList<User>) :
     RecyclerView.Adapter<UserAdapter.ViewHolder>() {
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val contactNameView = itemView.findViewById<TextView>(R.id.contact_name)
         val contactCareerView = itemView.findViewById<TextView>(R.id.contact_career)
         val contactImageView = itemView.findViewById<ImageView>(R.id.user_photo)
+        val contactDeleteIcon = itemView.findViewById<ImageView>(R.id.delete_icon)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserAdapter.ViewHolder {
@@ -27,18 +29,39 @@ class UserAdapter(var contactsList: List<User>) :
     }
 
     override fun onBindViewHolder(holder: UserAdapter.ViewHolder, position: Int) {
+
+        val screenHeight = holder.itemView.context.resources.displayMetrics.heightPixels
+        // TODO export magic number to a constant
+        val cardHeight = (screenHeight * 0.1).toInt()
+
+        holder.itemView.layoutParams.height = cardHeight
+
         val user: User = contactsList[position]
 
         val contactNameField = holder.contactNameView
         val contactCareerField = holder.contactCareerView
         val contactImageField = holder.contactImageView
+        val contactDeleteIcon = holder.contactDeleteIcon
 
         contactNameField.text = user.name
         contactCareerField.text = user.career
         contactImageField.loadImageByGlide(user.photo)
+        contactDeleteIcon.setOnClickListener{ deleteItem(position, holder) }
     }
 
     override fun getItemCount(): Int {
         return contactsList.size
+    }
+
+    fun deleteItem(index: Int, holder: ViewHolder) {
+        contactsList.removeAt(index)
+        notifyDataSetChanged()
+        // TODO export string
+        showToast("contact has been removed", holder)
+    }
+
+    private fun showToast(message: String, holder: ViewHolder) {
+        val context = holder.itemView.context.applicationContext
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 }
