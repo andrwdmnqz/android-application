5f2cac30800965cfc743eee99d5a3858cadfe39e
Andrew <andresshokhirev@gmail.com>
2024-03-28 09:50:06 +0200

    fixes after branch merging

--- app/src/main/java/com/project/myproject/RegisterActivity.kt
+++ app/src/main/java/com/project/myproject/RegisterActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Patterns
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.textfield.TextInputEditText
