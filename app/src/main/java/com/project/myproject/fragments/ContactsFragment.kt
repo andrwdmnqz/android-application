a044b0463dca5331732b84477f629024a2990d3b
Andrew <andresshokhirev@gmail.com>
2024-04-13 08:58:22 +0300

    implemented back arrow

--- app/src/main/java/com/project/myproject/fragments/ContactsFragment.kt
+++ app/src/main/java/com/project/myproject/fragments/ContactsFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -46,9 +48,18 @@ class ContactsFragment : Fragment(R.layout.fragment_contacts), UserAdapter.OnDel
 
         setupRecyclerView()
 
+        setupBackArrowListeners()
+
         setupAddContactListeners()
 
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun setupBackArrowListeners() {
+
+        binding.toolbarBack.setOnClickListener {
+            it.findNavController().popBackStack()
+        }
     }
 
     private fun setupAddContactListeners() {
--- app/src/main/java/com/project/myproject/fragments/DetailViewFragment.kt
+++ app/src/main/java/com/project/myproject/fragments/DetailViewFragment.kt
@@ -6,40 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.FragmentNavigatorExtras
 import com.project.myproject.R
-import com.project.myproject.SettingPreference
-import com.project.myproject.databinding.FragmentMainBinding
-import kotlinx.coroutines.launch
+import com.project.myproject.databinding.FragmentDetailViewBinding
 
 class DetailViewFragment : Fragment(R.layout.fragment_detail_view) {
 
-    private var _binding: FragmentMainBinding? = null
+    private var _binding: FragmentDetailViewBinding? = null
     private val binding get() = _binding!!
-
-    private lateinit var settingPreference: SettingPreference
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentMainBinding.inflate(layoutInflater, container, false)
-
-        settingPreference = SettingPreference(requireContext())
+        _binding = FragmentDetailViewBinding.inflate(layoutInflater, container, false)
 
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        initializeLogoutButtonListeners()
+        setupBackArrowListeners()
 
-        initializeContactsButtonListeners()
-
-        parseName()
+        setupTextFields()
 
         setupAnimation()
 
@@ -59,28 +49,14 @@ class DetailViewFragment : Fragment(R.layout.fragment_detail_view) {
         super.onDestroyView()
     }
 
-    private fun initializeLogoutButtonListeners() {
+    private fun setupBackArrowListeners() {
 
-        binding.logoutButton.setOnClickListener {
-            lifecycleScope.launch {
-                settingPreference.clearData()
-
-                val extras = FragmentNavigatorExtras(binding.topRectangle to "registerBackground")
-                it.findNavController().navigate(
-                    MainFragmentDirections.actionMainFragmentToRegisterFragment(), extras)
-            }
+        binding.toolbarBack.setOnClickListener {
+            it.findNavController().popBackStack()
         }
     }
 
-    private fun initializeContactsButtonListeners() {
-
-        binding.contactsButton.setOnClickListener {
-
-            it.findNavController().navigate(R.id.action_mainFragment_to_contactsFragment)
-        }
-    }
-
-    private fun parseName() {
+    private fun setupTextFields() {
         var name = MainFragmentArgs.fromBundle(requireArguments()).email
         val nameField = binding.name
 
